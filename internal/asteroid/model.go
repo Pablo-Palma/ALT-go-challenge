abb90ab89baeb254a471d9333a45d839beda16ad
Pablo Palma Rodríguez <pablopalmarodriguez@iMac-de-Pablo.local>
2024-05-30 12:33:46 +0200

    We have the app and its methods working and adjustment in model.go

--- internal/asteroid/model.go
+++ internal/asteroid/model.go
@@ -1,12 +1,12 @@
 package asteroid
 
 type Asteroid struct {
-	ID					string	`json:"id"`
-	Name				string	`json:"name"`
-	Diameter			float64	`json:"diameter"`
-	DiscoveryDate		string	`json:"discovery_date!"`
-	ObservationCount	string	`json:"observation_count"`
-	Distances			[]string	`json:"distances, omitempty"`
+	ID					string		`json:"id"`
+	Name				string		`json:"name"`
+	Diameter			float64		`json:"diameter"`
+	DiscoveryDate		string		`json:"discovery_date"`
+	Observations		string		`json:"observations, omitempty"`
+	Distances			[]Distance	`json:"distances, omitempty"`
 }
 
 type Distance struct {
